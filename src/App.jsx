d74aa3a2f292ed4a7dd4613161a538320583868c
Nignignina01~ <ninetta.inchiappa@gmail.com>
2023-07-03 18:05:30 +0200

    feat:sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="block_1">
+          <h2> block 1</h2>
+          <div className="rectangular_1"> </div>
+          <div className="rectangular_2"> </div>
+          <div className="rectangular_3"> </div>
+        </div>
+
+        <div className="block_2">
+          <h2> block 1</h2>
+          <div className="circle_1"> </div>
+          <div className="circle_2"> </div>
+          <div className="circle_3"> </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/scss/variables/geometries.scss";
+        @import "./src/scss/variables/color.scss";
+        @import "./src/scss/variables/gradient.scss";
+        @import "./src/scss/function/function.scss";
+        @import "./src/scss/variables/device.scss";
+        `,
+      },
+    },
+  },
+});
